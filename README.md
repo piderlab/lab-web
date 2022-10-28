da3c0646014feb36dcbe2eaa440866919a8f8dcd
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-10-28 17:26:34 +0900

    add markdown server

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# lab-web
+# 畑研究室のホームページ
+
+[信州大学工学部 畑研究室](https://www.shinshu-u.ac.jp/faculty/engineering/eict/introduction/teacher/hata-hideaki.php)の公式Webサイトです。
+
+## 研究
+
+- [Denoの外部モジュールについての実証分析](./deno_research.md)
+
+## 研究室の様子
+
+現在6人の学生が活動しています！
+
+![研究室の様子](./assets/lab.png)
+
+## リンク
+
+- [GitHub](https://github.com/piderlab/)
+- [「信州大産」電力でＥＶシェア構想　乗りたい人のオークション形式で実験へ](https://www.shinmai.co.jp/news/article/CNTS2022010900118)
+
+<br>
+<br>
--- serve.ts
+++ serve.ts
@@ -1,4 +1,44 @@
 import { serve } from "https://deno.land/std@0.160.0/http/mod.ts";
 import { serveDir } from "https://deno.land/std@0.160.0/http/file_server.ts";
+import { contentType } from "https://deno.land/std@0.160.0/media_types/mod.ts";
+import { CSS, render } from "https://deno.land/x/gfm@0.1.26/mod.ts";
 
-serve((req) => serveDir(req));
+const html = (markdown: string) => `
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+      main {
+        max-width: 800px;
+        margin: 0 auto;
+      }
+      ${CSS}
+      img{
+        width: 80%;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+      }
+    </style>
+  </head>
+  <body>
+    <main data-color-mode="light" data-light-theme="light" data-dark-theme="dark" class="markdown-body">
+      ${render(markdown)}
+    </main>
+  </body>
+</html>
+`;
+
+serve(async (req) => {
+  const res = await serveDir(req);
+  const { pathname } = new URL(req.url);
+  if (pathname.endsWith(".md")) {
+    return new Response(html(await res.text()), {
+      headers: { ...res.headers, "Content-Type": contentType(".html") },
+    });
+  } else {
+    return res;
+  }
+});
