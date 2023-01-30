aa3e764323c702c96399fec73ae67ed0b1ac548e
ayame113 <40050810+ayame113@users.noreply.github.com>
2023-01-30 16:33:45 +0900

    fix: 研究室名を修正 (#10)

--- serve.ts
+++ serve.ts
@@ -18,7 +18,7 @@ const html = (
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>畑研究室 - ${title || ""}</title>
+    <title>${title || ""} | 信州大学 実証的ソフトウェア工学研究室</title>
     <style>
       header {
         max-width: 800px;
